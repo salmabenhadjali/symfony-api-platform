14684bffb201ad7ece9c4ced6ec2067bf987ad68
sbenhadjali <salma.benhadjali@veolia.com>
2023-01-09 17:44:40 +0100

    feat: User can view his full profile (including email and roles) - custom Normalizer
    link: use Symfony\Component\Serializer\SerializerInterface;

---
+++ project/src/Serializer/UserAttributeNormaliser.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: salmabha
+ * Date: 09/01/2023
+ * Time: 17:16
+ */
+
+namespace App\Serializer;
+
+
+use App\Entity\User;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+use Symfony\Component\Serializer\Exception\LogicException;
+use Symfony\Component\Serializer\Normalizer\ContextAwareNormalizerInterface;
+use Symfony\Component\Serializer\Normalizer\NormalizableInterface;
+use Symfony\Component\Serializer\SerializerAwareInterface;
+use Symfony\Component\Serializer\SerializerAwareTrait;
+
+class UserAttributeNormaliser implements ContextAwareNormalizerInterface, SerializerAwareInterface
+{
+    use SerializerAwareTrait;
+
+    const USER_ATTRIBUTE_NORMALIZER_ALREADY_CALLED = 'USER_ATTRIBUTE_NORMALIZER_ALREADY_CALLED';
+
+    /**
+     * @var TokenStorageInterface
+     */
+    private $tokenStorage;
+
+    public function __construct(TokenStorageInterface $tokenStorage)
+    {
+        $this->tokenStorage = $tokenStorage;
+    }
+
+    public function supportsNormalization($data, $format = null, array $context = [])
+    {
+        if (isset($context[self::USER_ATTRIBUTE_NORMALIZER_ALREADY_CALLED])) {
+            return false;
+        }
+
+        return $data instanceof User;
+    }
+
+    public function normalize($object, $format = null, array $context = [])
+    {
+        if ($this->isUserHimself($object)) {
+            $context['groups'][] = 'get-owner';
+        }
+
+        // Now continue with serialization
+        return $this->passOn($object, $format, $context);
+    }
+
+    private function isUserHimself($object)
+    {
+        return $object->getUsername() === $this->tokenStorage->getToken()->getUsername();
+    }
+
+    private function passOn($object, $format, array $context)
+    {
+        if (!$this->serializer instanceof NormalizableInterface) {
+            throw new LogicException(
+                sprintf(
+                    'Cannot normailize object "%s" beacause the injected serializer is not a normalizer.',
+                    $object
+                )
+            );
+        }
+
+        $context[self::USER_ATTRIBUTE_NORMALIZER_ALREADY_CALLED] = true;
+
+        return $this->serializer->normalize($object, $format, $context);
+    }
+}
