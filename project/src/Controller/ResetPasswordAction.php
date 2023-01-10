d7d4b3ccdb89e8ca7269aca71b13422ddc5be055
sbenhadjali <salma.benhadjali@veolia.com>
2023-01-10 16:25:21 +0100

    feat: Implementing custom PasswordReset action
    link: https://birdz.udemy.com/course/symfony-api-platform-reactjs-full-stack-masterclass/learn/lecture/12294654#overview

--- project/src/Controller/ResetPasswordAction.php
+++ project/src/Controller/ResetPasswordAction.php
@@ -11,6 +11,10 @@ namespace App\Controller;
 
 use ApiPlatform\Validator\ValidatorInterface;
 use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class ResetPasswordAction
 {
@@ -18,24 +22,49 @@ class ResetPasswordAction
      * @var ValidatorInterface
      */
     private $validator;
+    /**
+     * @var UserPasswordEncoderInterface
+     */
+    private $userPasswordEncoder;
+    /**
+     * @var EntityManagerInterface
+     */
+    private $entityManager;
+    /**
+     * @var JWTTokenManagerInterface
+     */
+    private $tokenManager;
 
-    public function __construct(ValidatorInterface $validator)
+    public function __construct(
+        ValidatorInterface $validator,
+        UserPasswordEncoderInterface $userPasswordEncoder,
+        EntityManagerInterface $entityManager,
+        JWTTokenManagerInterface $tokenManager
+    )
     {
         $this->validator = $validator;
+        $this->userPasswordEncoder = $userPasswordEncoder;
+        $this->entityManager = $entityManager;
+        $this->tokenManager = $tokenManager;
     }
 
     public function __invoke(User $data)
     {
-        //$reset = new ResetPasswordAction();
-        //$reset();
-//        var_dump(
-//            $data->getNewPassword(),
-//            $data->getNewRetypedPassword(),
-//            $data->getOldPassword(),
-//            $data->getRetypedPassword()
-//        );die;
+        $this->validator->validate($data);
+
+        $data->setPassword(
+            $this->userPasswordEncoder->encodePassword($data, $data->getNewPassword())
+        );
+
+        $this->entityManager->flush();
+
+        $token = $this->tokenManager->create($data);
+
+        return new JsonResponse(['token' => $token]);
 
         //validator is only called after we return the data from this action!
-        $this->validator->validate($data);
+        //Only here it checks for user current password, but we've just modified it!
+
+        //Entity is persisted automatically, only if validation pass
     }
 }
